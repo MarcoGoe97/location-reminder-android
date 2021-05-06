cc7545080e987cb356054a86dfb9a5bc6a34eb1c
Marco Goedeckemeyer <marco@goedeckemeyer.com>
2021-05-06 21:03:53 +0200

    show error message when permission denied

--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -196,6 +196,8 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback{
         if (requestCode == REQUEST_LOCATION_PERMISSION) {
             if (grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                 enableMyLocation()
+            } else {
+                _viewModel.showSnackBar.value = getString(R.string.permission_denied_explanation)
             }
         }
     }
